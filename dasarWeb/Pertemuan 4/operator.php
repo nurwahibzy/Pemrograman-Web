ccdff18f1ab64449cee5c94504a2f6b9054ddcac
Nurwahibzy <nurwahibzakkiy@gmail.com>
2024-09-21 10:48:17 +0700

    Praktikum 3 : Operator | Soal 3.1

---
+++ dasarWeb/Pertemuan 4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil Tambah dari $a dan $b adalah $hasilTambah <br>";
+echo "Hasil Kurang dari $a dan $b adalah $hasilKurang <br>";
+echo "Hasil Kali dari $a dan $b adalah $hasilKali <br>";
+echo "Hasil Bagi dari $a dan $b adalah $hasilBagi <br>";
+echo "Sisa bagi dari $a dan $b adalah $sisaBagi <br>";
+echo "Hasil pangkat dari $a pangkat $b adalah $pangkat <br>";
+?>
