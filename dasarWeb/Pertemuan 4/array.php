d15f13c1755b1fbb734620f327c7994084420968
Nurwahibzy <nurwahibzakkiy@gmail.com>
2024-09-21 13:23:54 +0700

    Soal 5.4

--- dasarWeb/Pertemuan 4/array.php
+++ dasarWeb/Pertemuan 4/array.php
@@ -51,3 +51,36 @@ echo "Daftar nilai mahasiswa dalam mata kuliah $mataKuliah: <br>";
 foreach ($daftarNilai[$mataKuliah] as $nilai) {
     echo "Nama: {$nilai[0]}, Nilai: {$nilai[1]} <br>";
 }
+
+echo "<br>";
+echo "<br>";
+echo "Ada soal cerita : <br>
+Seorang guru ingin mencetak daftar nilai siswa dalam ujian matematika. <br>
+Guru tersebut memiliki data setiap siswa terdiri dari nama dan nilai. <br>
+Bantu guru ini mencetak daftar nilai siswa yang mencapai nilai di atas rata-rata kelas. <br>
+Dengan ketentuan nama dan nilai siswa Alice dapat 85, Bob dapat 92, Charlie dapat 78, David dapat 64, Eva dapat 90";
+
+$daftarNilaiMtk = [
+    ['Alice', 85],
+    ['Bob', 92],
+    ['Charlie', 78],
+    ['David', 64],
+    ['Eva', 90],
+];
+
+$jumlahNilai = 0;
+foreach ($daftarNilaiMtk as $nilaiMtk) {
+    $jumlahNilai += $nilaiMtk[1];
+}
+$rataRata = $jumlahNilai / count($daftarNilaiMtk);
+echo "<br>";
+echo "nilai rata-rata adalah ; $rataRata";
+echo "<br>";
+
+foreach ($daftarNilaiMtk as $nilaiMtk) {
+   if ($nilaiMtk[1] > $rataRata) {
+    echo "$nilaiMtk[0] dengan nilai $nilaiMtk[1] <br>";
+   }
+}
+
+echo "<br>";
