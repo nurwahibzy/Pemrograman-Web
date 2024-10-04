9b7a1259614873fd553a6e11f6a3ff905599a682
Wahib <nurwahibzakkiy@gmail.com>
2024-10-04 09:46:19 +0700

    Soal 19

---
+++ dasarWeb/Pertemuan 6/date.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <h3>date</h3>
+    <?php
+    echo "Today is ". date("Y/m/d"). "<br>";
+    echo "Today is ". date("Y.m.d"). "<br>";
+    echo "Today is ". date("Y-m-d"). "<br>";
+    echo "Today is ". date("l"). "<br>";
+    ?>
+
+</body>
+</html>
