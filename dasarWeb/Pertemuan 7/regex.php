27924f73a96a9d4627f3c17bb690dc7f6472bc62
Wahib <nurwahibzakkiy@gmail.com>
2024-10-16 20:26:00 +0700

    Soal 5.6

--- dasarWeb/Pertemuan 7/regex.php
+++ dasarWeb/Pertemuan 7/regex.php
@@ -42,3 +42,13 @@ if (preg_match($pattern, $text, $matches)) {
 } else {
     echo "Tidak ada yang cocok!";
 }
+
+echo "<br>";
+$pattern = '/go{1,2}d/';
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
