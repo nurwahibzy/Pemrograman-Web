8ae76f9e58b77e8ba0d2ac187a52b1d06a8803b5
Wahib <nurwahibzakkiy@gmail.com>
2024-10-16 20:03:38 +0700

    Soal 4.2

--- dasarWeb/Pertemuan 7/html_aman.php
+++ dasarWeb/Pertemuan 7/html_aman.php
@@ -10,6 +10,8 @@
     <form method="POST" action="">
         <label for="input">Masukkan teks:</label>
         <input type="text" name="input" id="input">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" ><br><br>
         <input type="submit" value="Submit">
     </form>
 
@@ -19,6 +21,14 @@
         $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 
         echo $input ;
+
+        $email = $_POST['email'];
+        if (filter_var($email,FILTER_VALIDATE_EMAIL)) {
+            echo "<p>Email anda valid: " ."<br>". htmlspecialchars($email, ENT_QUOTES, 'UTF-8') . "</p>";
+        } else {
+            echo "email anda tidak valid";
+        }
+        
     }
     ?>
 </body>
