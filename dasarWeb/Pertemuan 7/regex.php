38956f3ba069b5874ca67db7652b98638f5546f2
Wahib <nurwahibzakkiy@gmail.com>
2024-10-16 20:14:40 +0700

    Soal 5.2

--- dasarWeb/Pertemuan 7/regex.php
+++ dasarWeb/Pertemuan 7/regex.php
@@ -3,6 +3,15 @@ $patern = '/[a-z]/';
 $text = 'This is a Sample Text.';
 if (preg_match($patern, $text)) {
     echo "Huruf kecil ditemukan!";
-}else{
+} else {
     echo "Tidak ada huruf kecil!";
+}
+
+echo "<br>";
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+$text = 'There are 123 apples.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "\n";
+} else {
+    echo "Tidak ada yang cocok!" . "\n";
 }
