5e170fc3405904b0367c5ef5daf9d0598ab9343c
Wahib <nurwahibzakkiy@gmail.com>
2024-10-18 08:58:02 +0700

    Soal 5

--- dasarWeb/Pertemuan 8/upload.php
+++ dasarWeb/Pertemuan 8/upload.php
@@ -9,6 +9,7 @@ if (isset($_POST["submit"])) {
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah.";
+            echo "<br>gambar yang anda unggah :" . '<img src="'.$targetfile.'" width="200px">';
         } else {
             echo "Gagal mengunggah file.";
         }
