a17bf78c6e592c339504049d2cd35f3bd3338ede
Wahib <nurwahibzakkiy@gmail.com>
2024-10-20 16:05:00 +0700

    Soal 9

--- dasarWeb/Pertemuan 8/form_multiupload.php
+++ dasarWeb/Pertemuan 8/form_multiupload.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Unggah dokumen</h2>
     <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="files[]" multiple="multiple" accept=".pdf,.doc,.docx">
+        <input type="file" name="files[]" multiple="multiple" accept=".jpg, .jpeg, .png">
         <input type="submit" value="Unggah">
     </form>
 </body>
