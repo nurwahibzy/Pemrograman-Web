f340dc8053a6b832416c18c8f0bb34b4cd5e67a7
Wahib <nurwahibzakkiy@gmail.com>
2024-10-23 20:44:08 +0700

    Soal 17, 18, 19

---
+++ dasarWeb/Pertemuan 8/keranjangBelanja.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <h2> Keranjang Belanja</h2>
+        <?PHP 
+        $beliNovel = $_COOKIE['beliNovel'];
+        $beliBuku = $_COOKIE['beliBuku'];
+
+        echo "Jumlah Novel :". $beliNovel;
+        echo "Jumlah Buku :". $beliBuku;
+        ?>
+    </body>
+</html>
---
+++ dasarWeb/Pertemuan 8/prosesBeli.php
@@ -0,0 +1,6 @@
+<?PHP
+if (isset($_POST["beliNovel"]) && isset($_POST["beliBuku"])) {
+    setcookie("beliNovel", $_POST['beliNovel']);
+    setcookie("beliBuku", $_POST['beliBuku']);
+    header("location:keranjangBelanja.php");
+}
